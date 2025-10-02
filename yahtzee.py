e85e9ae82db73a9c017ce2d1158fdd711fc8c284
Francisco <franciscolmccerdeira@gmail.com>
2025-10-02 21:26:23 +0100

    4

--- yahtzee.py
+++ yahtzee.py
@@ -61,6 +61,64 @@ def has_straight(dice, length):
             longest_seq = 1
     return False
 
+def evaluate(dice):
+    """Calculate all possible scores given a dice roll"""
+
+    score_dictionary = {} #where all possible scores are appended
+    dice_roll_frequency = helper_freq_dict_for_list(dice) #frequency of all digits in the dice roll
+
+    #sum all the digits individually in the dice_roll
+    for digit in range(1, len(dice)):
+        if digit in dice_roll_frequency:
+            score_dictionary[str(digit)] = dice_roll_frequency[digit] * digit
+        else:
+            score_dictionary[str(digit)] = 0
+
+    #three of a kind, four of a kind 
+    #ACHO QUE ISTO TÁ MAL
+    for digit in dice_roll_frequency:
+        if dice_roll_frequency[digit] >= 3:
+            score_dictionary["three_of_a_kind"] = sum(dice)
+        else:
+            score_dictionary["three_of_a_kind"] = 0
+
+        if dice_roll_frequency[digit] >= 4:
+            score_dictionary["four_of_a_kind"] = sum(dice)
+        else:
+            score_dictionary["four_of_a_kind"] = 0
+
+    #full house
+    if len(dice_roll_frequency.keys()) == 2 and sorted(dice_roll_frequency.values()) == [2, 3]: #if we sort them we dont have to iterate over them 2 times 
+        score_dictionary["full_house"] = 25
+    else:
+        score_dictionary["full_house"] = 0
+    
+    #four_straight, five_straight
+    if has_straight(dice, 5):
+        score_dictionary["four_straight"] = 30
+        score_dictionary["five_straight"] = 40
+    else:
+        score_dictionary["five_straight"] = 0
+        if has_straight(dice, 4):
+            score_dictionary["four_straight"] = 30
+        else:
+            score_dictionary["four_straight"] = 0
+    
+    #yahtzee
+    if len(dice_roll_frequency.keys()) == 1:
+        score_dictionary["yahtzee"] = 50
+    else:
+        score_dictionary["yahtzee"] = 0
+    
+    #chance
+    score_dictionary["chance"] = sum(dice)
+
+    return score_dictionary
+
+
+
+
+
 if __name__ == "__main__":
     print(roll_dice.__doc__)
     print(roll_dice(5))
@@ -77,3 +135,15 @@ if __name__ == "__main__":
     print(y)
     print(has_straight.__doc__)
     print(has_straight([1,2,3,4],4))
+    x=[2, 3, 3, 5, 6]
+    print(x)
+    print(evaluate(x))
+    x=[1, 1, 1, 1, 1]
+    print(x)
+    print(evaluate(x))
+    x=[2, 3, 3, 5, 6]
+    print(x)
+    print(evaluate(x))
+    x=[2, 2, 2, 2, 5]
+    print(x)
+    print(evaluate(x))
